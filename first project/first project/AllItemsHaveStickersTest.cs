b34c12e34a6fc9a9fdaebcf5bf8900d93ac0b615
Olga Savinova <zayolya@gmail.com>
2016-11-28 15:51:43 +0300

    Задание 8. Тест, проверяющий что у всех элементов на главной странице есть стикеры.

---
+++ first project/first project/AllItemsHaveStickersTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace first_project
+{
+    [TestClass]
+    public class AllItemsHaveStickersTest : TestBase 
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            _driver.Url = "http://localhost:81/litecart/";
+            ReadOnlyCollection<IWebElement> items = _driver.FindElements(By.XPath("//div[@class='content']//ul[@class='listing-wrapper products']//li"));
+            foreach (var item in items)
+            {
+                ReadOnlyCollection<IWebElement> stickers = item.FindElements(By.CssSelector("div[class^='sticker']"));
+                Assert.AreEqual(1,stickers.Count);
+            }
+           
+        }
+    }
+}
